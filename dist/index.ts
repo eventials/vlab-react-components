42ee8d9e55ffecf26343a5ff18e94b6a1b44237b
pietrobs <pietro.schiavinato@unesp.br>
2020-09-22 09:44:19 -0300

    Alteração componentes

--- dist/index.ts
+++ dist/index.ts
@@ -1,3 +1,9 @@
-import TextInput from "./components/TextInput";
+import Logo from "./components/Logo";
+import VlabLogo from "./components/VlabLogo";
+import Button from "./components/Button";
+import Typography from "./components/Typography";
+import PageHeader from "./components/PageHeader";
+import FilterButton from "./components/FilterButton";
+import Topbar from "./components/Topbar";
 
-export { TextInput };
+export { Logo, VlabLogo, Button, Typography, PageHeader, FilterButton,Topbar };
