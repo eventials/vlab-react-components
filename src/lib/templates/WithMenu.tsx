9583d144cb9333c0b9e3487a4e0d20ed79b600f3
pietrobs <pietro.schiavinato@unesp.br>
2020-09-25 14:32:39 -0300

    fix template WithMenu
    Se nenhuma rota é passada, o menu lateral não será exibido

--- src/lib/templates/WithMenu.tsx
+++ src/lib/templates/WithMenu.tsx
@@ -59,45 +59,48 @@ const WithMenu = ({ children, routes }: any) => {
 
     return (
         <StyledLayout>
-            <Topbar hamburgerAction={toggleCollapsed} collapsed={collapsed} />
+            <Topbar hamburgerAction={toggleCollapsed} collapsed={collapsed && routes} />
             <Layout>
-                <Sider
-                    trigger={null}
-                    width={250}
-                    breakpoint="md"
-                    collapsedWidth="0"
-                    collapsible
-                    collapsed={collapsed}
-                    onBreakpoint={broken => {
-                        console.log(broken);
-                    }}
-                    onCollapse={(collapsed, type) => {
-                        console.log(collapsed, type);
-                    }}
-                    style={{
-                        backgroundColor: 'white',
-                        height: '100vh',
-                        overflow: 'auto',
-                        position: 'fixed',
-                        left: 0,
-                        top: 56,
-                    }}
-                >
-                    <Menu style={{ height: '100vh' }} theme="light" defaultSelectedKeys={['0']}>
-                        {routes.map((route: IRoute, index: number) => {
+                {
+                    routes &&
+                    <Sider
+                        trigger={null}
+                        width={250}
+                        breakpoint="md"
+                        collapsedWidth="0"
+                        collapsible
+                        collapsed={collapsed}
+                        onBreakpoint={broken => {
+                            console.log(broken);
+                        }}
+                        onCollapse={(collapsed, type) => {
+                            console.log(collapsed, type);
+                        }}
+                        style={{
+                            backgroundColor: 'white',
+                            height: '100vh',
+                            overflow: 'auto',
+                            position: 'fixed',
+                            left: 0,
+                            top: 56,
+                        }}
+                    >
+                        <Menu style={{ height: '100vh' }} theme="light" defaultSelectedKeys={['0']}>
+                            {routes.map((route: IRoute, index: number) => {
 
-                            return (
-                                <Menu.Item onClick={beforeClick(route)} key={String(index)} icon={<FireOutlined />}>
-                                    {route.name}
-                                </Menu.Item>)
-                        })}
-                    </Menu>
-                </Sider>
-                <Layout style={{ marginLeft: collapsed ? 0 : 250, marginTop: 56 }}>
+                                return (
+                                    <Menu.Item onClick={beforeClick(route)} key={String(index)} icon={<FireOutlined />}>
+                                        {route.name}
+                                    </Menu.Item>)
+                            })}
+                        </Menu>
+                    </Sider>
+                }
+                <Layout style={{ marginLeft: collapsed || !routes ? 0 : 250, marginTop: 56 }}>
                     <Content>
                         {children}
                     </Content>
-                    <Footer style={{ textAlign: 'center' }}>VLab Components ©2020 Created by VLab Team</Footer>
+                    <Footer style={{ textAlign: 'center' }}>VLab ©2020</Footer>
                 </Layout>
             </Layout>
         </StyledLayout>
