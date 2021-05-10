066672d2bed1fe03e8dbe772fd09b8feef4ef190
pietrobs <pietro.schiavinato@unesp.br>
2021-05-10 13:39:50 -0300

    Add typography custom class

--- src/lib/components/Typography.tsx
+++ src/lib/components/Typography.tsx
@@ -1,41 +1,46 @@
-import React from 'react';
-import styled from 'styled-components';
-import colorsList, { Colors } from '../theme/colors';
-import typographyList, { TypographyTypes } from '../theme/typography';
-import '../theme/typography.css';
+import React from "react";
+import styled from "styled-components";
+import colorsList, { Colors } from "../theme/colors";
+import typographyList, { TypographyTypes } from "../theme/typography";
+import "../theme/typography.css";
 
-const StyledTypography = styled.span`
+const StyledTypography = styled.span``;
 
-`;
-
-export interface ITypography{
-    type?: TypographyTypes;
-    color?: Colors;
-    children: any;
+export interface ITypography {
+  type?: TypographyTypes;
+  className?: string;
+  color?: Colors;
+  children: any;
 }
 
-const DEFAULT_TYPOGRAPHY = 'body1';
-const DEFAULT_COLOR = 'darkGray';
+const DEFAULT_TYPOGRAPHY = "body1";
+const DEFAULT_COLOR = "darkGray";
 
 const getTypographyType = (type: TypographyTypes | undefined) => {
-    if(!type || !typographyList[type]) return typographyList[DEFAULT_TYPOGRAPHY];
-    
-    // if(!typographyList[type]){
-    //     throw new Error(`Invalid typography type: ${type}!`);
-    // }
-    return typographyList[type];
-}
+  if (!type || !typographyList[type]) return typographyList[DEFAULT_TYPOGRAPHY];
+
+  // if(!typographyList[type]){
+  //     throw new Error(`Invalid typography type: ${type}!`);
+  // }
+  return typographyList[type];
+};
 
 const getColor = (color: Colors | undefined) => {
-    if(!color || !colorsList[color]) return colorsList[DEFAULT_COLOR];
+  if (!color || !colorsList[color]) return colorsList[DEFAULT_COLOR];
 
-    // if(!colorsList[color]){
-    //     throw new Error(`Invalid color type: ${color}!`);
-    // }
-    return colorsList[color];
-}
+  // if(!colorsList[color]){
+  //     throw new Error(`Invalid color type: ${color}!`);
+  // }
+  return colorsList[color];
+};
 
-
-const Typography = ({type, color, children}: ITypography) => <StyledTypography className="vlab-typography" style={{...getTypographyType(type), color: getColor(color) }}>{children}</StyledTypography>
+const Typography = ({ type, color, children, className }: ITypography) => (
+  <StyledTypography
+    className={`vlab-typography ${className}`}
+    style={{ ...getTypographyType(type), color: getColor(color) }}
+  >
+    {children}
+  </StyledTypography>
+);
 
 export default Typography;
