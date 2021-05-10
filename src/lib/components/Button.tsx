58ab2cb13902cbd74e195132db356fdab8a907ef
pietrobs <pietro.schiavinato@unesp.br>
2021-05-10 13:39:44 -0300

    Add button onlyIcon

--- src/lib/components/Button.tsx
+++ src/lib/components/Button.tsx
@@ -1,10 +1,10 @@
-import { Spin } from 'antd';
-import { TooltipProps } from 'antd/lib/tooltip';
-import React from 'react';
-import styled from 'styled-components';
-import { Tooltip, Typography } from '..';
-import colorsList, { Colors } from '../theme/colors';
-import { LoadingOutlined } from '@ant-design/icons';
+import { Spin } from "antd";
+import { TooltipProps } from "antd/lib/tooltip";
+import React from "react";
+import styled from "styled-components";
+import { Tooltip, Typography } from "..";
+import colorsList, { Colors } from "../theme/colors";
+import { LoadingOutlined } from "@ant-design/icons";
 
 interface StyledButtonProps {
   type?: ButtonType;
@@ -12,12 +12,12 @@ interface StyledButtonProps {
   size?: ButtonSize;
   fullWidth?: boolean;
   disabled?: boolean;
+  onlyIcon?: boolean;
 }
 
-type ButtonType = 'primary' | 'secondary' | 'terciary' | 'error' | 'success';
-type ButtonVariant = 'contained' | 'outlined' | 'text';
-type ButtonSize = 'small' | 'medium' | 'large';
-
+type ButtonType = "primary" | "secondary" | "terciary" | "error" | "success";
+type ButtonVariant = "contained" | "outlined" | "text";
+type ButtonSize = "small" | "medium" | "large";
 
 export interface IButtonProps {
   type?: ButtonType;
@@ -27,6 +27,7 @@ export interface IButtonProps {
   disabled?: boolean;
   loading?: boolean;
 
+  onlyIcon?: boolean;
   leftIcon?: any;
   rightIcon?: any;
 
@@ -36,17 +37,26 @@ export interface IButtonProps {
   children?: any;
 }
 
-const getBackgroundColor = ({ type, variant, disabled }: StyledButtonProps): string => {
+const getBackgroundColor = ({
+  type,
+  variant,
+  disabled,
+}: StyledButtonProps): string => {
   if (disabled) return colorsList.lightGray;
 
-  if (variant === "outlined" || variant === "text") return colorsList.transparent;
+  if (variant === "outlined" || variant === "text")
+    return colorsList.transparent;
 
   if (!type) return colorsList.primary;
 
   return colorsList[type];
-}
+};
 
-const getBorderColor = ({ type, variant, disabled }: StyledButtonProps): string => {
+const getBorderColor = ({
+  type,
+  variant,
+  disabled,
+}: StyledButtonProps): string => {
   if (disabled) return colorsList.lightGray;
 
   if (variant === "outlined" && type) return colorsList[type];
@@ -54,39 +64,50 @@ const getBorderColor = ({ type, variant, disabled }: StyledButtonProps): string 
   if (variant === "outlined" && !type) return colorsList.primary;
 
   return colorsList.transparent;
-}
+};
 
-const getHoverBackgroundColor = ({ type, variant, disabled }: StyledButtonProps): string => {
+const getHoverBackgroundColor = ({
+  type,
+  variant,
+  disabled,
+}: StyledButtonProps): string => {
   if (disabled) return colorsList.lightGray;
 
-  if (variant === 'outlined' || variant === 'text') return colorsList.lightBlue;
+  if (variant === "outlined" || variant === "text") return colorsList.lightBlue;
 
   switch (type) {
-    case 'primary': return colorsList.secondary;
-    case 'secondary': return colorsList.terciary;
-    default: return colorsList.terciary;
+    case "primary":
+      return colorsList.secondary;
+    case "secondary":
+      return colorsList.terciary;
+    default:
+      return colorsList.terciary;
   }
-}
+};
 
 const getColor = ({ type, variant, disabled }: StyledButtonProps): Colors => {
   if (disabled) return colorsList.regentGray;
   if ((variant === "outlined" || variant === "text") && type) return type;
 
-  return 'white';
-}
+  return "white";
+};
 
 const getSize = ({ size }: StyledButtonProps) => {
-  const SMALL_SIZE = '6px 20px';
-  const MEDIUM_SIZE = '12px 40px';
-  const LARGE_SIZE = '16px 50px';
+  const SMALL_SIZE = `6px 20px`;
+  const MEDIUM_SIZE = `12px 40px`;
+  const LARGE_SIZE = `16px 50px`;
 
   switch (size) {
-    case 'small': return SMALL_SIZE;
-    case 'medium': return MEDIUM_SIZE;
-    case 'large': return LARGE_SIZE;
-    default: return MEDIUM_SIZE;
+    case "small":
+      return SMALL_SIZE;
+    case "medium":
+      return MEDIUM_SIZE;
+    case "large":
+      return LARGE_SIZE;
+    default:
+      return MEDIUM_SIZE;
   }
-}
+};
 
 const StyledButton = styled.span<StyledButtonProps>`
   display: flex;
@@ -96,66 +117,86 @@ const StyledButton = styled.span<StyledButtonProps>`
   padding: ${getSize};
   margin: 5px;
 
-  width: ${({ fullWidth }) => fullWidth ? '100%' : 'auto'};
+  ${({ onlyIcon }) => (onlyIcon ? "padding-right: 0; padding-left: 0;" : "")};
+
+  width: ${({ fullWidth }) => (fullWidth ? "100%" : "auto")};
 
   background-color: ${getBackgroundColor};
   border: 1px solid ${getBorderColor};
-  border-radius: 5px; 
-  transition: .5s;
+  border-radius: 5px;
+  transition: 0.5s;
   cursor: pointer;
 
   text-decoration: none;
 
   ${({ variant }) => {
     switch (variant) {
-      case 'text':
+      case "text":
         return `
           border: none;
         `;
     }
   }}
 
-  .left-icon{
+  .left-icon {
     margin-right: 16px;
   }
 
-  .right-icon{
+  .right-icon {
     margin-left: 16px;
   }
 
-  .ant-spin{
+  .ant-spin {
     color: ${getColor};
     margin-right: 16px;
   }
 
-  &:hover{
+  &:hover {
     background-color: ${getHoverBackgroundColor};
   }
 `;
 
-const Button = ({ children, tooltipProps, leftIcon, rightIcon, ...props }: IButtonProps) => {
-
-  const antIcon = <LoadingOutlined style={{ fontSize: 24, color: colorsList[getColor(props)] }} spin />;
+const Button = ({
+  children,
+  tooltipProps,
+  leftIcon,
+  rightIcon,
+  onlyIcon,
+  ...props
+}: IButtonProps) => {
+  const antIcon = (
+    <LoadingOutlined
+      style={{ fontSize: 24, color: colorsList[getColor(props)] }}
+      spin
+    />
+  );
 
   return (
-    <Tooltip {...tooltipProps as TooltipProps} color={tooltipProps?.color ? colorsList[tooltipProps.color] : null}>
-      <StyledButton {...props} onClick={props.disabled || props.loading ? () => { } : props.onClick}>
-        {!props.loading && 
-        <div className="left-icon">
-          {leftIcon}
-        </div>
-        }
+    <Tooltip
+      {...(tooltipProps as TooltipProps)}
+      color={tooltipProps?.color ? colorsList[tooltipProps.color] : null}
+    >
+      <StyledButton
+        {...props}
+        onlyIcon={onlyIcon}
+        onClick={props.disabled || props.loading ? () => {} : props.onClick}
+      >
+        {!props.loading && <div className="left-icon">{leftIcon}</div>}
         <Typography type="button" color={getColor(props)}>
-          {props.loading && <Spin wrapperClassName="loading" spinning={props.loading} indicator={antIcon} delay={500}></Spin>}{children}
+          {props.loading && (
+            <Spin
+              wrapperClassName="loading"
+              spinning={props.loading}
+              indicator={antIcon}
+              delay={500}
+            ></Spin>
+          )}
+          {children}
         </Typography>
-        {!props.loading && 
-        <div className="right-icon">
-          {rightIcon}
-        </div>
-        }
+        {!props.loading && <div className="right-icon">{rightIcon}</div>}
       </StyledButton>
     </Tooltip>
-  )
+  );
 };
 
 export default Button;
